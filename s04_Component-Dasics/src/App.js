60b51b9fd7ca3c0041fb766ec133c1634bf0694f
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-21 18:46:06 +0200

    separate header

--- s04_Component-Dasics/src/App.js
+++ s04_Component-Dasics/src/App.js
@@ -27,16 +27,14 @@ const concepts = [
 function App() {
   return (
     <div>
-      <header>
-        <img src={keyConceptsImage} alt="Medal badge with a star" />
-        <h1>Key React Concepts</h1>
-        <p>Selected key React concepts you should know about</p>
-      </header>
-      <ul id="concepts">
-        <li className="concept">
-          <img src="TODO: IMAGE" alt="TODO: TITLE" />
-          <h2>TODO: TITLE</h2>
-          <p>TODO: DESCRIPTION</p>
+      <ul id='concepts'>
+        <li className='concept'>
+          <img
+            src={concepts[0].image}
+            alt={concepts[0].title}
+          />
+          <h2>{concepts[0].title}</h2>
+          <p>{concepts[0].description}</p>
         </li>
       </ul>
     </div>
