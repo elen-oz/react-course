6e7bffd1171185a303cea6fddbf0f438f687704c
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-25 20:21:21 +0200

    change message

--- s20_MultiPage SPA/src/pages/Error.js
+++ s20_MultiPage SPA/src/pages/Error.js
@@ -5,7 +5,7 @@ const ErrorPage = () => {
     <>
       <MainNavigation />
       <main>
-        <h1>An error occured!</h1>
+        <h1>An error occured</h1>
         <p>Could not find this page</p>
       </main>
     </>
