a2c5b191069e9871e17ab1b2269acb9c312a581e
Elena Ozerova <ozerova.lenka@gmail.com>
2023-07-05 18:30:40 +0200

    TS: ref

--- s28_TypeScript/src/components/NewTodo.tsx
+++ s28_TypeScript/src/components/NewTodo.tsx
@@ -1,6 +1,16 @@
+import { useRef } from 'react';
+
 const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    const enteredText = todoTextInputRef.current?.value;
+
+    if (enteredText?.trim().length === 0) {
+      return;
+    }
   };
 
   return (
@@ -9,6 +19,7 @@ const NewTodo = () => {
       <input
         type='text'
         id='text'
+        ref={todoTextInputRef}
       />
       <button>Add Todo</button>
     </form>
