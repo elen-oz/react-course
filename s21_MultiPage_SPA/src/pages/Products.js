3d44202ef72d1d084f3d6a5397d49a126c3b158d
Elena Ozerova <ozerova.lenka@gmail.com>
2023-07-06 18:16:24 +0200

    using dynamic routes

--- s21_MultiPage_SPA/src/pages/Products.js
+++ s21_MultiPage_SPA/src/pages/Products.js
@@ -1,5 +1,15 @@
 const ProductsPage = () => {
-  return <h1>The Products Page</h1>;
+  return (
+    <>
+      <h1>The Products Page</h1>
+      <ul>
+        <li></li>
+        <li></li>
+
+        <li></li>
+      </ul>
+    </>
+  );
 };
 
 export default ProductsPage;
