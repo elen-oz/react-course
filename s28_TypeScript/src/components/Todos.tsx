02d6b907c73a18164a580c4f31ed385ef779f9a8
Elena Ozerova <ozerova.lenka@gmail.com>
2023-07-06 10:44:21 +0200

    add styles

--- s28_TypeScript/src/components/Todos.tsx
+++ s28_TypeScript/src/components/Todos.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react';
 import TodoItem from './TodoItem';
 import Todo from '../models/todo';
+import classes from './Todos.module.css';
 
 const Todos: FC<{ items: Todo[] }> = (props) => {
   return (
-    <ul>
+    <ul className={classes.todos}>
       {props.items.map((item) => (
         <TodoItem
           key={item.id}
