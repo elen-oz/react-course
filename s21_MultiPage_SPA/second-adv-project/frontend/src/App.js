db57e9e138d096b65429a79787866f2a7cf930d9
Elena Ozerova <ozerova.lenka@gmail.com>
2023-07-10 21:54:43 +0200

    finish task

--- s21_MultiPage_SPA/second-adv-project/frontend/src/App.js
+++ s21_MultiPage_SPA/second-adv-project/frontend/src/App.js
@@ -7,6 +7,7 @@ import EventsPage from './pages/EventsPage';
 import EventDetailPage from './pages/EventDetailPage';
 import NewEventPage from './pages/NewEventPage';
 import EditEventPage from './pages/EditEventPage';
+import EventsRootLayout from './pages/EventsRootLayout';
 
 const router = createBrowserRouter([
   {
@@ -14,11 +15,17 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     // errorElement: <ErrorPage />,
     children: [
-      { path: '', element: <HomePage /> },
-      { path: 'events', element: <EventsPage /> },
-      { path: 'events/:eventId', element: <EventDetailPage /> },
-      { path: 'events/new', element: <NewEventPage /> },
-      { path: 'events/:eventId/edit', element: <EditEventPage /> },
+      { index: true, element: <HomePage /> },
+      {
+        path: 'events',
+        element: <EventsRootLayout />,
+        children: [
+          { index: true, element: <EventsPage /> },
+          { path: ':eventId', element: <EventDetailPage /> },
+          { path: 'new', element: <NewEventPage /> },
+          { path: ':eventId/edit', element: <EditEventPage /> },
+        ],
+      },
     ],
   },
 ]);
