8adb22bee6df310a12a2279a70d5ae03cf3bfb94
Elena Ozerova <ozerova.lenka@gmail.com>
2023-07-11 15:00:35 +0200

    add useLoader hook

--- s21_MultiPage_SPA/second-adv-project/frontend/src/components/EventItem.js
+++ s21_MultiPage_SPA/second-adv-project/frontend/src/components/EventItem.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import classes from './EventItem.module.css';
 
 function EventItem({ event }) {
@@ -7,12 +9,15 @@ function EventItem({ event }) {
 
   return (
     <article className={classes.event}>
-      <img src={event.image} alt={event.title} />
+      <img
+        src={event.image}
+        alt={event.title}
+      />
       <h1>{event.title}</h1>
       <time>{event.date}</time>
       <p>{event.description}</p>
       <menu className={classes.actions}>
-        <a href="edit">Edit</a>
+        <Link to='edit'>Edit</Link>
         <button onClick={startDeleteHandler}>Delete</button>
       </menu>
     </article>
