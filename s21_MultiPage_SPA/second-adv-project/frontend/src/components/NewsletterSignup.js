c37fa6aaca2bf55a1600101caa21388d8519d1a0
Elena Ozerova <ozerova.lenka@gmail.com>
2023-07-11 17:31:26 +0200

    add useFetcher

--- s21_MultiPage_SPA/second-adv-project/frontend/src/components/NewsletterSignup.js
+++ s21_MultiPage_SPA/second-adv-project/frontend/src/components/NewsletterSignup.js
@@ -1,15 +1,34 @@
+import { useFetcher } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 import classes from './NewsletterSignup.module.css';
 
 function NewsletterSignup() {
+  const fetcher = useFetcher();
+  const { data, state } = fetcher;
+  const [inputValue, setInputValue] = useState(null);
+
+  useEffect(() => {
+    if (state === 'idle' && data && data.message) {
+      window.alert(data.message);
+      setInputValue('');
+    }
+  }, [data, state]);
+
   return (
-    <form method="post" className={classes.newsletter}>
+    <fetcher.Form
+      method='post'
+      action='/newsletter'
+      className={classes.newsletter}
+    >
       <input
-        type="email"
-        placeholder="Sign up for newsletter..."
-        aria-label="Sign up for newsletter"
+        type='email'
+        placeholder='Sign up for newsletter...'
+        aria-label='Sign up for newsletter'
+        value={inputValue}
       />
       <button>Sign up</button>
-    </form>
+    </fetcher.Form>
   );
 }
 
