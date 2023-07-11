fb49569d16d1fea728d70da7d73d746a056536ea
Elena Ozerova <ozerova.lenka@gmail.com>
2023-07-11 14:30:15 +0200

    refactor: json-utility function

--- s21_MultiPage_SPA/second-adv-project/frontend/src/pages/EventsPage.js
+++ s21_MultiPage_SPA/second-adv-project/frontend/src/pages/EventsPage.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, json } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
 
@@ -15,12 +15,13 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch('http://localhost:8080/ev1ents');
+  const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
     // return { isError: true, message: 'Error fetching events' };
     // throw new Error('Error fetching events');
-    throw new Response(JSON.stringify({ message: 'Error fetching events' }), { status: 500 });
+    // throw new Response(JSON.stringify({ message: 'Error fetching events' }), { status: 500 });
+    throw json({ message: 'Error fetching events' }, { status: 500 });
   } else {
     const resData = await response.json();
     return resData.events;
